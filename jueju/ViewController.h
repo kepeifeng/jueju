cc2aae77dfd43b1757fdc21aca099159988670fb
Kent Peifeng Ke <kepeifeng@gmail.com>
2015-11-19 11:28:59 +0800

    Initial Commit

---
+++ jueju/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jueju
+//
+//  Created by Kent on 11/19/15.
+//  Copyright © 2015 Kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ jueju/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  jueju
+//
+//  Created by Kent on 11/19/15.
+//  Copyright © 2015 Kent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
